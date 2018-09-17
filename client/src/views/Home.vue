699b08bacd43562819a20884fe4fcbfeb48566e8
Mikazuki <mikazuki_fuyuno@outlook.com>
2018-09-18 08:16:16 +0900

    fix(client): Home layout

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -7,33 +7,32 @@
      uk-img
     )
       h1.uk-heading-primary Atlas
-  //
-    section#featured.container
+    section.uk-section.uk-container
       div.uk-text-center(uk-grid)
         div(class="uk-width-1-4@l uk-width-1-2@s")
-          h2.title.is-4
+          h3.title.is-4
             div.far.fa-search.fa-2x
             | 素早くアクセス
           p
-            | タグを用いることで、探しているイラストに早く、簡単にアクセスできます。
+            | タグを用いることで、探しているイラストに早く、直感的かつ簡単にアクセスできます。
         div(class="uk-width-1-4@l uk-width-1-2@s")
-          h2.title.is-4
+          h3.title.is-4
             div.fab.fa-osi.fa-2x
             | オープンソース
           p
-            | 誰もが制限のない、自分のためだけのサーバーを建てることが出来ます。
+            | 誰もが検閲や制限のない、自分のためだけのサーバーを建てることができます。
         div(class="uk-width-1-4@l uk-width-1-2@m")
-          h2.title.is-4
+          h3.title.is-4
             div.fas.fa-user-secret.fa-2x
             | 自由なアクセス
           p
-            | アップロードしたイラストには、アクセス権限を自由に設定することが出来ます。
+            | アップロードしたイラストには、自由にアクセス制限をかけることができます。
         div(class="uk-width-1-4@l uk-width-1-2@m")
-          h2.title.is-4
+          h3.title.is-4
             div.fal.fa-usd-circle.fa-2x
             | 無料
           p
-            | 誰もが無料で、全ての機能を制限されることなく使うことが出来ます。
+            | 全ての人が無料で、一切の機能を制限されることなく使うことができます。
 </template>
 
 
