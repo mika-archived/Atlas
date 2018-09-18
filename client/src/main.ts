ff2fdf09f2bd844698cf9e7b9b7d14579c985d61
Mikazuki <mikazuki_fuyuno@outlook.com>
2018-09-19 08:50:20 +0900

    chore(client): register hooks

--- client/src/main.ts
+++ client/src/main.ts
@@ -3,6 +3,8 @@ import Vue from "vue";
 import "tslib";
 import "uikit";
 
+import "./classComponentHooks";
+
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
