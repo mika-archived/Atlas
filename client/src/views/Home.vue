77ebf8192963c390d725a78d8deda2e5d0bd2b59
Mikazuki <mikazuki_fuyuno@outlook.com>
2018-09-20 20:01:39 +0900

    fix(client): Remove test code

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -2,7 +2,6 @@
   div
     template(v-if="hasSession")
       explorer
-      gravatar(:email="email")
     template(v-else)
       featured
 </template>
@@ -14,27 +13,14 @@ import { Getter } from "vuex-class";
 
 import Explorer from "../components/Home/Explorer.vue";
 import Featured from "../components/Home/Featured.vue";
-import Gravatar from "../presentationals/Gravatar.vue";
-
-import { ISession } from "../models/session";
-import { Nullable } from "../models/types";
 
 @Component({
   components: {
     Explorer,
-    Featured,
-    Gravatar
+    Featured
   }
 })
 export default class Navigation extends Vue {
   @Getter("hasSession") public hasSession!: boolean;
-  @Getter("getSession") public getSession!: Nullable<ISession>;
-
-  public get email(): string {
-    if (this.hasSession) {
-      return (this.getSession as ISession).attributes.email;
-    }
-    return "";
-  }
 }
 </script>
