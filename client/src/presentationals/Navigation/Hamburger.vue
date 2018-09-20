5a697d99daf772b891c493bb8cd3294c06f629f1
Mikazuki <mikazuki_fuyuno@outlook.com>
2018-09-20 19:59:12 +0900

    chore(client): Off-Canvas Navigation

--- client/src/presentationals/Navigation/Hamburger.vue
+++ client/src/presentationals/Navigation/Hamburger.vue
@@ -1,12 +1,41 @@
 <template lang="pug">
   div
-    ul.uk-navbar-nav(class="uk-visible@m") 
+    ul.uk-navbar-nav(class="uk-visible@s") 
       slot(content)
-    ul.uk-navbar-nav(class="uk-hidden@m")
+    ul.uk-navbar-nav(class="uk-hidden@s")
       li
-        a.uk-navbar-toggle
+        a.uk-navbar-toggle(uk-toggle="target: #offcanvas-overlay")
           span(uk-navbar-toggle-icon)
-        .uk-navbar-dropdown
-          ul.uk-nav.uk-navbar-dropdown-nav
+    #offcanvas-overlay(uk-offcanvas="overlay: true")
+      .uk-offcanvas-bar
+        .uk-panel
+          ul.uk-nav.uk-nav-default
+            li.uk-nav-header General
             slot(content)
 </template>
+
+<style lang="scss" scoped>
+.uk-offcanvas-bar {
+  background: #fff;
+  color: #666;
+
+  .uk-nav-default {
+    .uk-nav-header {
+      padding: 8px 0;
+      border-bottom: 1px solid #e5e5e5;
+    }
+
+    li {
+      padding: 4px 0;
+    }
+
+    * {
+      &:active,
+      &:hover {
+        color: #666;
+      }
+      color: #666;
+    }
+  }
+}
+</style>
