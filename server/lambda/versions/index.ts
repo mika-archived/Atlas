9d88603d59ca92aa799a237f702ce3de2d2b8d24
Mikazuki <mikazuki_fuyuno@outlook.com>
2018-09-23 05:38:35 +0900

    chore(server/lambda): Update

---
+++ server/lambda/versions/index.ts
@@ -0,0 +1,18 @@
+import { APIGatewayEvent } from "aws-lambda";
+
+import { IResponse } from "../shared/types";
+
+exports.handler = async (event: APIGatewayEvent) => {
+  const response = {
+    body: JSON.stringify({
+      versions: ["v1"]
+    }),
+    headers: {
+      "access-control-allow-credentials": "true",
+      "access-control-allow-origin": "*"
+    },
+    statusCode: 200,
+  } as IResponse;
+
+  return response;
+};
