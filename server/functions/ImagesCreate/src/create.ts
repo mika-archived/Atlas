6387cfb2b42f6a8685b1b6dccff7e5e4731fc23a
Mikazuki <mikazuki_fuyuno@outlook.com>
2018-09-24 23:37:34 +0900

    chore(server): Resize image

--- server/functions/ImagesCreate/src/create.ts
+++ server/functions/ImagesCreate/src/create.ts
@@ -1,21 +1,77 @@
 import { APIGatewayEvent } from "aws-lambda";
 import { DynamoDB, S3 } from "aws-sdk";
-import { resize } from "imagemagick";
+import * as fs from "fs";
+import * as im from "imagemagick";
 import { v4 as uuid } from 'uuid';
 
 import { createResponse } from "@/response";
 
+const regex = new RegExp(/data:image\/([a-z]+);base64,(.*)/);
+
+function resize(options: any): Promise<any> {
+  return new Promise((resolve, reject) => {
+    im.resize(options, (err, res) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(res);
+      }
+    });
+  });
+}
+
 exports.handler = async (event: APIGatewayEvent) => {
-  const json = JSON.parse(event.body as string);
-  if (!json.image) {
+  const { base64Image, ...json } = JSON.parse(event.body as string);
+  if (!base64Image) {
     return createResponse(400, { message: "Invalid Arguments" });
   }
 
-  // retrieve
-  const { allowed, attributes, restrict } = json;
+  const matches = regex.exec(base64Image);
+  if (!matches) {
+    return createResponse(400, { message: "Unknown format" });
+  }
+
+  const extension = matches[1];
   const storageId = uuid();
+
+  // write to tmp directory
+  const srcPath = `/tmp/${uuid()}.${extension}`;
+  const buffer = new Buffer(matches[2], "base64");
+  fs.writeFileSync(srcPath, buffer);
+
+  // resize image (max 350x350)
+  const args = {
+    srcPath,
+    dstPath: `/tmp/${storageId}.${extension}`,
+    height: 350,
+    width: 350,
+    customArgs: [
+      "-auto-orient",
+      "-limit memory 96MB",
+      "-limit disk 0"
+    ]
+  };
+
+  await resize(args);
+
+  const img = new Buffer(fs.readFileSync(args.dstPath)).toString("base64");
+  try {
+    fs.unlinkSync(args.dstPath);
+  } catch {
+    // ignore
+  }
+
+  return createResponse(200, {
+    storageId,
+    base64Encoded: img
+  });
+
+  // retrieve
+  /*
+  const { allowed, attributes, restrict } = json;
 
   return createResponse(200, {
     storageId,
   });
+  */
 };
