51c48b35019dbb32afb42633b33120508b9c7b01
Mikazuki <mikazuki_fuyuno@outlook.com>
2018-09-29 02:13:38 +0900

    feat(client): Show placeholder when cannot display images

--- client/src/components/Home/Explorer.vue
+++ client/src/components/Home/Explorer.vue
@@ -4,7 +4,13 @@
       form.uk-search.uk-search-default
         span(uk-search-icon)
         input.uk-search-input.width-1-1(type="search" placeholder="イラストを検索...")
-
+    section
+      .uk-placeholder.uk-text-center
+        | 画像がまだアップロードされていません。
+        br
+        | ページ上の何処かに画像をドラッグ＆ドロップすることで、画像をアップロードできます。
+        br
+        | いくつか画像をアップロードすることで、このエリアに画像が表示されます。
 </template>
 
 <style lang="scss" scoped>
