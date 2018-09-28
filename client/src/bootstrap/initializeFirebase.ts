a005a75d94ff5035aaafe040022e884ea6cf72d1
Mikazuki <mikazuki_fuyuno@outlook.com>
2018-09-29 03:23:06 +0900

    chore(client): Change auth server

--- client/src/bootstrap/initializeFirebase.ts
+++ client/src/bootstrap/initializeFirebase.ts
@@ -2,7 +2,7 @@ import Firebase from "firebase";
 
 Firebase.initializeApp({
   apiKey: "AIzaSyCt8ykbYVv2dMG44enqk0gJCSZ6QLzsihg",
-  authDomain: "atlas-1537035161848.firebaseapp.com",
+  authDomain: "auth.atlas.mochizuki.moe",
   databaseURL: "https://atlas-1537035161848.firebaseio.com",
   projectId: "atlas-1537035161848",
   storageBucket: "atlas-1537035161848.appspot.com",
