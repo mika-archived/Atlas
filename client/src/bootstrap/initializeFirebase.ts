f8dfd79c8a9da5bde813366f49e1e6e6c7a9a578
Mikazuki <mikazuki_fuyuno@outlook.com>
2018-10-01 19:28:22 +0900

    chore: suppress warnings

--- client/src/bootstrap/initializeFirebase.ts
+++ client/src/bootstrap/initializeFirebase.ts
@@ -1,10 +1,14 @@
-import Firebase from "firebase";
+import firebase from "firebase";
 
-Firebase.initializeApp({
+firebase.initializeApp({
   apiKey: "AIzaSyCt8ykbYVv2dMG44enqk0gJCSZ6QLzsihg",
   authDomain: "auth.atlas.mochizuki.moe",
   databaseURL: "https://atlas-1537035161848.firebaseio.com",
   projectId: "atlas-1537035161848",
   storageBucket: "storage.atlas.mochizuki.moe",
   messagingSenderId: "478882579120"
+});
+
+firebase.firestore().settings({
+  timestampsInSnapshots: true
 });
--- server/functions/src/bootstrap/initializeFirebase.ts
+++ server/functions/src/bootstrap/initializeFirebase.ts
@@ -1,7 +1,11 @@
-import Firebase from "firebase-admin";
+import firebase from "firebase-admin";
 
-Firebase.initializeApp({
+firebase.initializeApp({
   databaseURL: "https://atlas-1537035161848.firebaseio.com",
   projectId: "atlas-1537035161848",
   storageBucket: "storage.atlas.mochizuki.moe",
+});
+
+firebase.firestore().settings({
+  timestampsInSnapshots: true
 });
