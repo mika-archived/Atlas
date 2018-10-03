bd390b6de07617eada19a3e4696eb17453e15568
Mikazuki <mikazuki_fuyuno@outlook.com>
2018-10-04 08:33:36 +0900

    feat(client): Illust details page

--- client/src/store/images.ts
+++ client/src/store/images.ts
@@ -4,44 +4,69 @@ import { firebaseAction } from "vuexfire";
 
 import { IImage } from "@/models/image";
 import { currentUser } from "@/models/session";
+import { Nullable } from "@/models/types";
 
 // tslint:disable no-shadowed-variable
 const store = firestore();
 
+export interface IGetImageParams {
+  id: string;
+}
+
 export interface IImagesState {
   images: [];
+  image: Nullable<IImage>;
 }
 
 interface IImagesActions {
-  getImages: { key: string };
+  getImages: {};
+  getImage: IGetImageParams;
 }
 
 interface IImagesGetters {
   images: IImage[];
+  image: Nullable<IImage>;
 }
 
 // tslint:disable:no-empty-interface
-interface IImagesMutations { /* */ }
+interface IImagesMutations {
+  clearImage: {};
+}
 
 const state: IImagesState = {
-  images: []
+  images: [],
+  image: null,
 };
 
 const actions: DefineActions<IImagesActions, IImagesState, IImagesMutations, IImagesGetters> = {
   async getImages(ctx, payload) {
-    firebaseAction<typeof ctx, typeof payload>(async ({ bindFirebaseRef, getters }, { }) => {
+    firebaseAction<typeof ctx, typeof payload>(async ({ bindFirebaseRef }) => {
       const user = await store.collection("users").doc((await currentUser()).uid).get();
       const query = store.collection("images").where("user", "==", user.ref).orderBy("timestamp", "desc").limit(60);
       bindFirebaseRef("images", query);
     })(ctx, payload);
   },
+
+  async getImage(ctx, payload) {
+    ctx.commit("clearImage", {});
+
+    firebaseAction<typeof ctx, typeof payload>(async ({ bindFirebaseRef }, { id }) => {
+      const image = await store.collection("images").doc(id);
+      bindFirebaseRef("image", image);
+    })(ctx, payload);
+  }
 };
 
 const getters: DefineGetters<IImagesGetters, IImagesState> = {
   images: state => state.images,
+  image: state => state.image,
 };
 
-const mutations: DefineMutations<IImagesMutations, IImagesState> = {};
+const mutations: DefineMutations<IImagesMutations, IImagesState> = {
+  clearImage(state) {
+    state.image = null;
+  }
+};
 
 export default {
   actions,
--- client/src/views/images/Show.vue
+++ client/src/views/images/Show.vue
@@ -1,26 +1,73 @@
 <template lang="pug">
-  .lightbox
-    .uk-container.uk-container-expand(uk-sticky="sel-target: .uk-navbar-container; cls-active: uk-navbar-sticky")
-      nav.uk-navbar-container(uk-navbar)
-        .uk-navbar-left
-          router-link.uk-navbar-item.uk-logo(to="/")
-            img(src="../../assets/logo.png" height="40px")
+  .uk-container
+    // mobile
+    .container(class="uk-hidden@m")
+      ul(uk-tab)
+        li
+          a(href="#") Image
+        li
+          a(href="#") Info
+      ul.uk-switcher
+        li
+          cloud-image.img(:image="image" mode="master")
+    // tablet ~
+    .container(class="uk-visible@m")
+      div(uk-grid)
+        div(class="uk-width-2-3@m uk-width-3-4@l uk-width-4-5@xl")
+          cloud-image.img(:image="image" mode="master")
 
+        div(class="uk-width-1-3@m uk-width-1-4@l uk-width-1-5@xl")
+          
+          h4 Access Restrict
+
+          h4 Tags
+
+          h4 Information
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
+import { Action, Getter } from "vuex-class";
 
-@Component
-export default class Show extends Vue {
-  public created(): void {
-    const body = document.querySelector("body") as HTMLBodyElement;
-    body.style.overflowY = "hidden";
+import { IImage } from "../../models/image";
+import { ActionDescriber } from "../../models/types";
+import CloudImage from "../../presentationals/CloudImage.vue";
+import { IGetImageParams } from "../../store/images";
+
+@Component({
+  components: {
+    "cloud-image": CloudImage
   }
+})
+export default class Show extends Vue {
+  @Getter("image")
+  public image!: IImage;
 
-  public destroyed(): void {
-    const body = document.querySelector("body") as HTMLBodyElement;
-    body.style.overflowY = "auto";
+  @Action("getImage")
+  public getImage!: ActionDescriber<IGetImageParams>;
+
+  public async created(): Promise<void> {
+    if (!this.$route.params.id) {
+      this.$router.push("/404");
+      return;
+    }
+    this.getImage({ id: this.$route.params.id });
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.uk-container {
+  width: calc(100% - 30px);
+
+  @media (min-width: 640px) {
+    width: calc(100% - 60px);
+  }
+}
+
+/deep/ img {
+  max-height: calc(100vh - 80px);
+  object-fit: contain;
+  width: 100%;
+}
+</style>
