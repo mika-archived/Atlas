31c3bba0482b62a12ee9328da4a999c21702b56b
Mikazuki <mikazuki_fuyuno@outlook.com>
2018-10-07 00:06:25 +0900

    chore(functions): Add credentials

--- server/functions/src/bootstrap/initializeFirebase.ts
+++ server/functions/src/bootstrap/initializeFirebase.ts
@@ -1,9 +1,13 @@
 import firebase from "firebase-admin";
 
+// tslint:disable-next-line:no-var-requires
+const credential = firebase.credential.cert(require("../../service-account-key.json"));
+
 firebase.initializeApp({
   databaseURL: "https://atlas-1537035161848.firebaseio.com",
   projectId: "atlas-1537035161848",
   storageBucket: "storage.atlas.mochizuki.moe",
+  credential,
 });
 
 firebase.firestore().settings({
