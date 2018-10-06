3ef6ec04f3336c572ac93ab67dc8fb6e38e713b2
Mikazuki <mikazuki_fuyuno@outlook.com>
2018-10-07 02:32:27 +0900

    fix(client): When resize to size is larger than original, miss convert

--- client/src/models/utils.ts
+++ client/src/models/utils.ts
@@ -1,12 +1,31 @@
 import { ISize } from "./types";
 
 // 入力サイズからアスペクト比を保持したまま、長い方が指定されたサイズになるようにする
-export function resizeSquare(orig: ISize, to: number): ISize {
+export function resizeSquare(original: ISize, to: number): ISize {
   if (to === 0) {
     return { width: 0, height: 0 };
   }
-  if (orig.height < orig.width) {
-    return { width: to, height: (orig.height * to / orig.width) };
+
+  // 幅の方が大きい
+  if (original.height < original.width) {
+    if (original.width <= to) {
+      return original;
+    }
+    return { width: to, height: (original.height * to / original.width) };
   }
-  return { width: (orig.width * to / orig.height), height: to };
+
+  // 同じ
+  if (original.height === original.width) {
+    if (original.height <= to) {
+      return original;
+    } else {
+      return { width: to, height: to };
+    }
+  }
+
+  // 高さの方が大きい
+  if (original.height <= to) {
+    return original;
+  }
+  return { width: (original.width * to / original.height), height: to };
 }
